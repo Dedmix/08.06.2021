a64eed346edc18d83c6e98d0c07142bda469fe4e
Dedmix <84181853+Dedmix@users.noreply.github.com>
2021-06-08 13:46:04 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp21
+{
+	class Program 
+	{
+		static void Main(string[] args)
+		{
+			MyList<int> spisok = new MyList<int>();
+
+			spisok.Add(new Node<int>(10));
+			spisok.Add(new Node<int>(20));
+			spisok.Add(new Node<int>(30));
+			spisok.Add(new Node<int>(40));
+			spisok.Add(new Node<int>(50));
+
+			spisok.PushFront(new Node<int>(1));
+
+
+			Console.WriteLine();
+			spisok.Print();
+			Console.WriteLine("Первый элемент списка (не считая который мы добавили)");
+			Console.WriteLine(spisok.Get(1));
+			Console.WriteLine();
+			Console.WriteLine();
+			spisok.Remove(4);
+			spisok.Print();
+			Console.WriteLine("Первый элемент списка (не считая который мы добавили)");
+			Console.WriteLine(spisok.Get(4));
+			
+
+
+		}
+	}
+}
